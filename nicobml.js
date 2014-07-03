01b9078d2bf66dbb0ce0b4e759a2650e70c923d1
quno <qunoster@gmail.com>
2014-07-03 18:53:49 +0900

    script update.

--- nicobml.js
+++ nicobml.js
@@ -1,55 +1,53 @@
 (function(d){
-var e = d.getElementById('bn_gbox').getElementsByTagName('meta');
-var date_from = trimDate(new Date(e[1].content));
-var date_to = new Date(date_from.getTime() + 3600000);
-var meta = d.getElementsByTagName('head')[0].getElementsByTagName('meta');
-var title;
-var c = 0;
-while(meta.length > c + 1){
-	if(meta[c].getAttribute("property") === 'og:title'){
-		title = meta[c].content;
-		break;
-	}
-	c++;
+try{
+	//エラー処理
+	if(location.toString().indexOf('live.nicovideo.jp/watch/lv') < 0 && location.toString().indexOf('live.nicovideo.jp/gate/lv') < 0) throw 'このURLでは無効です。';
+	var date_published = d.querySelector('meta[itemprop="datePublished"]');
+	if(date_published === null) throw '現在は使用できません。';
+	//開演時間
+	var date_from = trimDate(new Date(date_published.content));
+	//終了時間（1時間後）
+	var date_to = new Date(date_from.getTime() + 3600000);
+	//生放送タイトル
+	var title = d.querySelector('meta[property="og:title"]').content;
+	//Googleカレンダーに移動
+	d.location = getURL(date_from, date_to, title);
+} catch(e){
+	console.error(e);
 }
+//開場時間を開演時間に変更（10分前、3分前、30分前に対応）
 function trimDate(date){
-	var minite = date.getMinutes();
-	var new_date = date;
-	switch(minite) {
+	switch(date.getMinutes()) {
 		case 20:
 		case 50:
-			new_date = new Date(date.getTime() + 600000);
-			break;
+			return new Date(date.getTime() + 600000);
 		case 27:
 		case 57:
-			new_date = new Date(date.getTime() + 180000);
-			break;
+			return new Date(date.getTime() + 180000);
 		case 30:
-			new_date = new Date(date.getTime() + 1800000);
-			break;
+			return new Date(date.getTime() + 1800000);
+		default:
+			return date;
 	}
-	return new_date;
 }
-var getUTC = function(date){
+function getUTC(date){
+	function pad(n){return n<10 ? '0'+n : n}
 	return date.getUTCFullYear() +
-		zerofill(date.getUTCMonth()+1) +
-		zerofill(date.getUTCDate()) +
+		pad(date.getUTCMonth()+1) +
+		pad(date.getUTCDate()) +
 		'T' +
-		zerofill(date.getUTCHours()) +
-		zerofill(date.getUTCMinutes()) +
-		zerofill(date.getUTCSeconds()) +
+		pad(date.getUTCHours()) +
+		pad(date.getUTCMinutes()) +
+		pad(date.getUTCSeconds()) +
 		'Z';
-};
-var zerofill = function(num){
-	return ('0'+num).slice(-2);
-};
-var getURL = function(from, to, text){
-  return 'http://www.google.com/calendar/event?' +
+}
+function getURL(from, to, text){
+  return 'https://www.google.com/calendar/render?' +
   'action=' + 'TEMPLATE' +
   '&text='  + encodeURIComponent(text) +
   '&dates=' + getUTC(from) + '/' + getUTC(to) +
-  '&trp='   + 'false';
-  '&sprop=&sprop=name:';
+  '&details=' +
+  '&location=' +
+  '&trp=false&sf=true&output=xml';
 };
-d.location = getURL(date_from, date_to, title);
 })(document);
